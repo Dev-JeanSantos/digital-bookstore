53db574a4b61e10c25e8eafcaca54a1db3d0ff10
Dev-JeanSantos <jeancbsan@gmail.com>
2023-08-03 14:50:03 -0300

    implemented getByid and update

--- src/app.js
+++ src/app.js
@@ -16,10 +16,25 @@ app.get('/',(req, res) => {
 app.get('/books',(req, res) => {
     res.status(200).json(book)
 })
+app.get('/books/:id',(req, res) => {
+    let index = getBookById(req.params.id);
+    res.json(book[index]);
+})
 
 app.post('/books',(req, res) => {
     book.push(req.body);
     res.status(201).send(`Successfully registered book ${req.body.title}`)
 })
+
+app.put('/books/:id',(req, res) => {
+    let index = getBookById(req.params.id);
+    book[index].title = req.body.title
+    res.status(200).send(`Successfully update book ${req.body.title}`)
+})
+
+
+function getBookById(id){
+    return book.findIndex( book => book.id == id )
+}
 
 export default app;
