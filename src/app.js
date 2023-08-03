50823b292db25e5f5d437150813c5a4eab75dfde
Dev-JeanSantos <jeancbsan@gmail.com>
2023-08-03 14:37:00 -0300

    implemented endpoint createBook

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const book = [
-    {id:1, "titulo": "Dejavú"},
-    {id:2, "titulo": "O Livro de Eli"}
+    {id:1, "title": "Pais Brilhante, Filhos Fascinantes"},
+    {id:2, "title": "A Magia do Amor"}
 ]
 
 app.get('/',(req, res) => {
-    res.status(200).send('Curso de Node')
+    res.status(200).send('Home Page - Book API')
 })
 
 app.get('/books',(req, res) => {
     res.status(200).json(book)
+})
+
+app.post('/books',(req, res) => {
+    book.push(req.body);
+    res.status(201).send(`Successfully registered book ${req.body.title}`)
 })
 
 export default app;
