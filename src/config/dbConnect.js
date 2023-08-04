f692be2d194d85756fd4eb2fb8d63acaae7310da
Dev-JeanSantos <jeancbsan@gmail.com>
2023-08-04 14:41:40 -0300

    change and protected in variable of database

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://devjs:angela2008@node-js.tbubmhu.mongodb.net/devjs");
+mongoose.connect(process.env.STRING_CONEXAO_DB);
 let db = mongoose.connection
 
 export default db
